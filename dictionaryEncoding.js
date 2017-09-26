831578e3ff75a1fc87a5c420061a8b99a9f1cfc7
Jack-Flux <jflux.61744@farnborough.ac.uk>
2017-09-26 15:54:07 +0100

    Dictionary encoding

---
+++ dictionaryEncoding.js
@@ -0,0 +1,9 @@
+module.exports = (words) => {
+  let dictionary = {};
+  words.split('\n').forEach((line) => {
+    line.split(' ').forEach((word) => {
+      !dictionary[word] ? dictionary[word] = 1 : dictionary[word] + 1;
+    });
+  });
+  return dictionary
+}
