352724c32ffd52b02ed06396ecf4ea1408db1982
Jack-Flux <jflux.61744@farnborough.ac.uk>
2017-10-06 09:18:29 +0100

    Update dictionaryEncoding.js

--- dictionaryEncoding.js
+++ dictionaryEncoding.js
@@ -2,7 +2,7 @@ module.exports = (words) => {
   let dictionary = {};
   words.split('\n').forEach((line) => {
     line.split(' ').forEach((word) => {
-      !dictionary[word] ? dictionary[word] = 1 : dictionary[word] + 1;
+      !dictionary[word] ? dictionary[word] = 1 : dictionary[word] += 1;
     });
   });
   return dictionary
