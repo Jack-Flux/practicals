6e822b217cb1a1dccd833847ebcf9b7dd324a1f2
Jack-Flux <jflux.61744@farnborough.ac.uk>
2017-10-16 11:51:27 +0100

    Update runLengthEncoding.js

--- runLengthEncoding.js
+++ runLengthEncoding.js
@@ -1,12 +1,9 @@
 module.exports = (pattern) => {
   let numberCount = 0,
     previousNumber = 0,
-    totalZeros = 0,
-    totalOnes = 0,
     newPat = [];
 
   pattern.forEach((number) => {
-    number === 0 ? totalZeros += 1 : totalOnes +=1;
     numberCount += 1
     if(previousNumber !== number) {
       newPat.push(`${numberCount}x${previousNumber}`);
